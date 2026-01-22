d164ae3fe2ff0b515cbc9738c80257a2edac8024
GumanBehera15 <gumanbehera454@gmail.com>
2026-01-22 19:32:26 +0530

    Navbar Added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+        <Navbar />
+        {/* navbar*/}
+        {/* navbar*/}
+        {/* navbar*/}
+        {/* navbar*/}
+        {/* navbar*/}
+        {/* navbar*/}
+        {/* navbar*/}
+    </div>
+  )
+}
+
+export default App
